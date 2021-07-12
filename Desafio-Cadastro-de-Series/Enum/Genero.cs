44dc99c96feab6f1081fe4f48bef22d3476c92f4
Edson-cryptos <edsonlourenco935@gmail.com>
2021-07-12 01:44:05 -0300

    initial commit

---
+++ Desafio-Cadastro-de-Series/Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Biográfico= 1,
+		Aventura = 2,
+		Comedia = 3,
+		Comédia_dramática = 4,
+		Comédia_romântica = 5,
+		Terror = 6,
+		Faroeste = 7,
+		Fantasia = 8,
+		Ficcao_Cientifica = 9,
+		Acao = 10,
+		Romance = 11,
+		Suspense = 12,
+		Histórico = 13
+	}
+}
